9b0bd6818c08e3057c0be4950a785f88b7a95965
Mvhamad <bamohamed961@gmail.com>
2023-05-10 19:23:09 +0000

    datatable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import DataTable from 'react-data-table-component';
 import './App.css';
 
 function App() {
+  const colonne = [
+    {
+      name : "Prénom",
+      selector: row => row.prenom
+    },
+    {
+      name : "Nom",
+      selector: row => row.nom
+    },
+    {
+      name : "Numéro de Téléphone",
+      selector: row => row.tel
+    }
+  ];
+
+  const data = [
+    {
+      id : 1,
+      prenom : "Aladji",
+      nom : "Sall",
+      tel : 774561239
+    },
+    {
+      id : 2,
+      prenom : "Salif",
+      nom : "Sané",
+      tel : 777823439
+    },
+    {
+      id : 3,
+      prenom : "Penda",
+      nom : "BA",
+      tel : 785824756
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <DataTable columns={colonne} data={data}/>
     </div>
   );
 }
